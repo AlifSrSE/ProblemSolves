c876fdb82cfad2c364891b8ec655149db9d4637f
Alif Rahman <alifrahman@Alifs-Mac-mini.local>
2025-03-09 11:53:41 +0600

    1843E

--- 1843E-trackingSegments.py
+++ 1843E-trackingSegments.py
@@ -1,53 +1,42 @@
-6
-5 5
-1 2
-4 5
-1 5
-1 3
-2 4
-5
-5
-3
-1
-2
-4
-4 2
-1 1
-4 4
-2
-2
-3
-5 2
-1 5
-1 5
-4
-2
-1
-3
-4
-5 2
-1 5
-1 3
-5
-4
-1
-2
-3
-5
-5 5
-1 5
-1 5
-1 5
-1 5
-1 4
-3
-1
-4
-3
-3 2
-2 2
-1 3
-3
-2
-3
-1
+def check(n, l, r, x, change_num):
+    a = [0] * n
+    for i in range(change_num):
+        a[x[i] - 1] = 1
+
+    prefix_sums = [0] * n
+    for i in range(n):
+        prefix_sums[i] = (0 if i == 0 else prefix_sums[i-1]) + a[i]
+    
+    return any(
+        2 * (prefix_sums[r[i]-1] - (0 if l[i] == 1 else prefix_sums[l[i]-2])) > r[i] - l[i] + 1
+        for i in range(len(l))
+    )
+
+def solve(n, l, r, x):
+    result = -1
+    lower = 0
+    upper = len(x)
+    while lower <= upper:
+        middle = (lower + upper) // 2
+        if check(n, l, r, x, middle):
+            result = middle
+            upper = middle - 1
+        else:
+            lower = middle + 1
+    
+    return result
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    l = []
+    r = []
+    for _ in range(m):
+        li, ri = map(int, input().split())
+        l.append(li)
+        r.append(ri)
+    
+    q = int(input())
+    x = list(map(int, input().split()))
+    
+    print(solve(n, l, r, x))
