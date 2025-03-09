ebeec5e0ec48781603bc4ab5afbbe5c9be401ee5
Alif Rahman <alifrahman@Alifs-Mac-mini.local>
2025-03-09 11:17:03 +0600

    1843B

--- 1843C-sumInBinaryTree.py
+++ 1843C-sumInBinaryTree.py
@@ -1,7 +1,21 @@
-6
-3
-10
-37
-1
-10000000000000000
-15
+def sum_in_binary_tree(n):
+    total_sum = 0
+    while n > 0:
+        total_sum += n
+        n //= 2
+    return total_sum
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    t = int(data[0])
+    results = []
+    
+    for i in range(1, t + 1):
+        n = int(data[i])
+        results.append(sum_in_binary_tree(n))
+    
+    for result in results:
+        print(result)
