eeed725e7c6e507c7b7f1381adde94ef77aab2d7
alifrahman <alif.rahman.c@gmail.com>
2025-03-16 07:55:55 +0600

    1213F - unstable string sort

--- 1213F-unstableStringSort.py
+++ 1213F-unstableStringSort.py
@@ -1,38 +1,52 @@
 # Author : AlifSrSE
-# Date : 2025-03-15
-# Problem link : https://codeforces.com/contest/1213/problem/E
+# Date : 2025-03-14
+# Problem link : https://codeforces.com/contest/1213/problem/F
  
 import sys
  
-def restore_string(n, k, p, q):
-    s = [None] * n
-    rank = [0] * n
+def read():
+    return map(int, sys.stdin.readline().split())
+ 
+def solve():
+    n, m = read()
+    P = list(read())
+    Q = list(read())
+    pos = [0] * (n + 1)
+    L = [0] * (n + 1)
+    bel = [0] * (n + 1)
+    ans = [''] * (n + 1)
     
     for i in range(n):
-        rank[p[i] - 1] = i
-        rank[q[i] - 1] = max(rank[q[i] - 1], i)
+        pos[P[i]] = i + 1
     
-    distinct_count = 0
-    char_map = {}
-    result = [''] * n
+    k = n
+    for i in range(n - 1, -1, -1):
+        j = pos[Q[i]]
+        k = min(k, j)
+        L[j] = k
     
-    for i in sorted(range(n), key=lambda x: rank[x]):
-        if i == 0 or rank[i] != rank[i - 1]:
-            distinct_count += 1
-        char_map[i] = chr(97 + min(distinct_count - 1, k - 1))
+    j, k = 1, n
+    for i in range(n, 0, -1):
+        k = min(k, L[i])
+        bel[i] = j
+        if k == i:
+            j += 1
     
-    for i in range(n):
-        result[p[i] - 1] = char_map[i]
+    if bel[1] < m:
+        print("NO")
+        return
     
-    return "YES\n" + "".join(result)
+    print("YES")
+    j = -1
+    for i in range(1, n + 1):
+        if bel[i] != bel[i - 1]:
+            j += 1
+        ans[P[i - 1]] = chr(ord('a') + min(25, j))
+    
+    print("".join(ans[1:]))
  
 def main():
-    input = sys.stdin.read
-    data = list(map(int, input().split()))
-    n, k = data[0], data[1]
-    p = data[2:n+2]
-    q = data[n+2:2*n+2]
-    print(restore_string(n, k, p, q))
+    solve()
  
 if __name__ == "__main__":
     main()
