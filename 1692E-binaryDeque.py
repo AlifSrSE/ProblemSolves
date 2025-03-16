242d7ec42e06ff3ce57fb8e519f0dc3b152fac2f
alifrahman <alif.rahman.c@gmail.com>
2025-03-16 08:52:06 +0600

    1692E - binary deque

--- 1692E-binaryDeque.py
+++ 1692E-binaryDeque.py
@@ -1,41 +1,43 @@
 # Author : AlifSrSE
 # Date : 2025-03-16
 # Problem link : https://codeforces.com/contest/1692/problem/E
-
+ 
 from sys import stdin
 input = stdin.readline
-
+ 
 def main():
-    t = int(input().strip())  
+    t = int(input().strip())
     
     for _ in range(t):
         n, s = map(int, input().strip().split())
         a = list(map(int, input().strip().split()))
-
-        sum_total = sum(a)
-        ps = [0] * n
-        ps[0] = a[0]
-        for i in range(1, n):
-            ps[i] = ps[i-1] + a[i]
         
-        if s > sum_total:
+        if min(a) > s:
             print(-1)
-        elif s == sum_total:
-            print(0)
-        else:
-            ans = -1
-            for i in range(n):
-                if ps[i] == s:
-                    ans = max(ans, i + 1)
-                elif ps[i] > s:
-                    ds = ps[i] - s
-                    l = 0
-                    while l < i and ps[l] < ds:
-                        l += 1
-                    if ps[l] >= ds:
-                        ans = max(ans, i - l)
+            continue
             
-            print(n - ans)
-
+        curr_sum = 0
+        left = 0
+        max_len = 0
+        
+        for right in range(n):
+            curr_sum += a[right]
+            
+            while curr_sum > s and left <= right:
+                curr_sum -= a[left]
+                left += 1
+            
+            if curr_sum == s:
+                max_len = max(max_len, right - left + 1)
+            
+            if left == right + 1 and n - right - 1 + max_len <= max_len:
+                break
+        
+        total_sum = sum(a[left:]) + (curr_sum if left <= right else 0)
+        if total_sum < s or max_len == 0:
+            print(-1)
+        else:
+            print(n - max_len)
+ 
 if __name__ == "__main__":
     main()
