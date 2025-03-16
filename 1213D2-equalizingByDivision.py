26601c08b6a3e38e1ee5fc3b86f034a7c5d93dea
alifrahman <alif.rahman.c@gmail.com>
2025-03-16 07:52:41 +0600

    1213D2 - equalizing by division

---
+++ 1213D2-equalizingByDivision.py
@@ -0,0 +1,37 @@
+# Author : AlifSrSE
+# Date : 2025-03-14
+# Problem link : https://codeforces.com/contest/1213/problem/D2
+ 
+import sys
+from collections import defaultdict
+ 
+def min_operations(n, k, a):
+    a.sort()
+    kit = defaultdict(list)
+    
+    for num in a:
+        kit[num].append(0)
+        steps = 0
+        while num > 0:
+            num //= 2
+            steps += 1
+            kit[num].append(steps)
+    
+    ans = float('inf')
+    for it in kit.values():
+        if len(it) >= k:
+            it.sort()
+            ans = min(ans, sum(it[:k]))
+    
+    return ans
+ 
+def main():
+    input = sys.stdin.read
+    data = input().split()
+    n = int(data[0])
+    k = int(data[1])
+    a = list(map(int, data[2:]))
+    print(min_operations(n, k, a))
+ 
+if __name__ == "__main__":
+    main()
