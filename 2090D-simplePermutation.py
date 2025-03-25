2a6671c7cdc9702e65569730c6e7cd611471aec5
alifrahman <alif.rahman.c@gmail.com>
2025-03-25 09:11:04 +0600

    2090 series

--- 2090D-simplePermutation.py
+++ 2090D-simplePermutation.py
@@ -1,42 +1,56 @@
 # Author: AlifSrSe
-# date: 2025-03-23
+# date: 2025-03-25
 # https://codeforces.com/problemset/problem/2090/D
  
 import sys
- 
-input = sys.stdin.read
-data = input().split()
-index = 0
- 
-def get_int():
-    global index
-    val = int(data[index])
-    index += 1
-    return val
- 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
- 
-def solve(n):
-    perm = [2, 1] + list(range(3, n + 1))
-    c = []
-    s = 0
-    for i in range(n):
-        s += perm[i]
-        c.append((s + i) // (i + 1))
-    primes = sum(1 for x in c if is_prime(x))
-    target = (n // 3) - 1
-    if n > 2 and primes < target:
-        perm = list(range(n, 0, -1))
-    return perm
- 
-t = get_int()
-for _ in range(t):
-    n = get_int()
-    result = solve(n)
-    print(" ".join(map(str, result)))
+
+LIMIT = 200_000
+is_prime = [True] * (LIMIT + 1)
+
+def sieve_of_eratosthenes():
+    global is_prime
+    if LIMIT >= 1:
+        is_prime[1] = False
+
+    for i in range(2, int(LIMIT**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, LIMIT + 1, i):
+                is_prime[j] = False
+
+def get_permutation(num):
+    prime_pos = 1
+    for i in range(1, (2 * num) // 3 + 1):
+        if is_prime[i]:
+            prime_pos = i
+
+    sequence = [prime_pos]
+    left = prime_pos - 1
+    right = prime_pos + 1
+
+    while True:
+        if right > num:
+            break
+        sequence.append(right)
+        right += 1
+
+        if left < 1:
+            break
+        sequence.append(left)
+        left -= 1
+
+    for i in range(1, left + 1):
+        sequence.append(i)
+    for i in range(right, num + 1):
+        sequence.append(i)
+
+    print(" ".join(map(str, sequence)))
+
+def main():
+    sieve_of_eratosthenes()
+    t = int(sys.stdin.readline().strip())
+    for _ in range(t):
+        num = int(sys.stdin.readline().strip())
+        get_permutation(num)
+
+if __name__ == "__main__":
+    main()
