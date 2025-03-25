8661c560ef1929088005b4e6f2c7e121774d53b4
alifrahman <alif.rahman.c@gmail.com>
2025-03-25 09:34:38 +0600

    2037B

--- 2037B-interceptedInputs.py
+++ 2037B-interceptedInputs.py
@@ -2,25 +2,35 @@
 # date: 2025-03-24
 # https://codeforces.com/problemset/problem/2037/B
 
-def solve():
-    k = int(input())
-    a = list(map(int, input().split()))
-    m = {}
-    for i in a:
-        m[i] = m.get(i, 0) + 1
+import sys
+import math
+from collections import Counter
+
+def solve(k, a):
+    m = Counter(a) 
     v = k - 2
-    for i in range(1, int(v**0.5) + 1):
+    sqrt_v = int(math.sqrt(v))  
+    possible_divisors = set(m.keys()) & set(range(1, sqrt_v + 1))
+
+    for i in possible_divisors:
         if v % i == 0:
             vv = v // i
-            if i != vv:
-                if m.get(i, 0) and m.get(vv, 0):
-                    print(i, vv)
-                    return
-            else:
-                if m.get(i, 0) >= 2:
-                    print(i, vv)
-                    return
 
-t = int(input())
+            if vv in m:
+                print(i, vv)
+                return
+            elif i == vv and m[i] >= 2:
+                print(i, vv)
+                return
+
+input = sys.stdin.read
+data = input().split()
+t = int(data[0])
+
+index = 1
+results = []
 for _ in range(t):
-    solve()
+    k = int(data[index])
+    a = list(map(int, data[index + 1 : index + 1 + k]))
+    solve(k, a)
+    index += 1 + k
