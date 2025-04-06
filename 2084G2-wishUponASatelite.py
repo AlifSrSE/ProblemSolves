451c9dd155f23f60493c6c7ba6869b0ee2e42422
alifrahman <alif.rahman.c@gmail.com>
2025-04-06 10:02:14 +0600

    2084 solves

--- 2084G2-wishUponASatelite.py
+++ 2084G2-wishUponASatelite.py
@@ -1,92 +1,135 @@
 # Author: AlifSrSE
 # Email: alif.rahman.c@gmail.com
 
+import sys
+import random
+
+sys.setrecursionlimit(1 << 25)
+input = sys.stdin.readline
+
+MOD = int(1e9 + 7)
+N = 1 << 20
+
+class Node:
+    def __init__(self, val=0):
+        self.left = None
+        self.right = None
+        self.size = 1
+        self.weight = val
+        self.key = random.getrandbits(64)
+        self.fi = 0
+        self.ad = 0
+        self.left_val = val
+        self.right_val = val
+
+def update(x):
+    if not x:
+        return
+    x.size = 1 + (x.left.size if x.left else 0) + (x.right.size if x.right else 0)
+    x.left_val = x.left.left_val if x.left else x.weight
+    x.right_val = x.right.right_val if x.right else x.weight
+
+def add(x, l, h):
+    if not x:
+        return
+    left_size = x.left.size if x.left else 0
+    x.weight += l + h * left_size
+    x.fi += l
+    x.ad += h
+    x.left_val += l
+    x.right_val += l + h * (x.size - 1)
+
+def push(x):
+    if not x or (x.fi == 0 and x.ad == 0):
+        return
+    if x.left:
+        add(x.left, x.fi, x.ad)
+    if x.right:
+        left_size = x.left.size if x.left else 0
+        add(x.right, x.fi + x.ad * (left_size + 1), x.ad)
+    x.fi = x.ad = 0
+
+def split(root, t):
+    if not root:
+        return None, None
+    push(root)
+    left_size = root.left.size if root.left else 0
+    if root.weight <= 0:
+        x, root.left = split(root.left, t)
+        update(root)
+        return x, root
+    else:
+        root.right, y = split(root.right, t - left_size - 1)
+        update(root)
+        return root, y
+
+def merge(x, y):
+    if not x or not y:
+        return x or y
+    if x.key < y.key:
+        push(x)
+        x.right = merge(x.right, y)
+        update(x)
+        return x
+    else:
+        push(y)
+        y.left = merge(x, y.left)
+        update(y)
+        return y
+
+def dfs(x, cnt, ns):
+    if not x:
+        return cnt, ns
+    push(x)
+    cnt, ns = dfs(x.left, cnt, ns)
+    if cnt > 0:
+        ns += x.weight
+        cnt -= 1
+    cnt, ns = dfs(x.right, cnt, ns)
+    update(x)
+    return cnt, ns
+
 def solve():
     n = int(input())
     a = list(map(int, input().split()))
+    ia = [0] * (n + 1)
+    for i in range(n):
+        if a[i]:
+            ia[a[i]] = i + 1
 
-    zeros_count = a.count(0)
-    present_numbers = sorted([x for x in a if x != 0])
-    missing_numbers = sorted(list(set(range(1, n + 1)) - set(present_numbers)))
+    s0 = n // 2
+    s1 = (n + 1) // 2
+    cnt0 = cnt1 = num = 0
+    root = None
+    ND = 0
 
-    if zeros_count == 0:
-        def calculate_f(arr):
-            m = len(arr)
-            if m == 1:
-                return arr[0]
+    for i in range(n - 1, -1, -1):
+        if ia[i + 1]:
+            if ia[i + 1] % 2 == 0:
+                cnt0 += 1
+            else:
+                cnt1 += 1
+        else:
+            num += 1
+            ls, rs = split(root, 0)
+            root = merge(merge(ls, Node(0)), rs)
 
-            memo = {}
+        ND += (num + cnt0 + cnt1) * (num + cnt0 + cnt1 + 1) // 2
+        ND += cnt0 * (s0 - cnt0)
+        ND += (num + cnt1) * (s1 - num - cnt1)
 
-            def get_f_recursive(current_arr, turn):
-                current_tuple = (tuple(current_arr), turn)
-                if current_tuple in memo:
-                    return memo[current_tuple]
+        add(root, (s0 - 2 * cnt0) + (2 * num + 2 * cnt1 - s1) - 2, -4)
 
-                m_current = len(current_arr)
-                if m_current == 1:
-                    return current_arr[0]
+    ns = 0
+    cnt = s0 - cnt0
+    _, ns = dfs(root, cnt, ns)
 
-                results = []
-                for i in range(m_current - 1):
-                    next_arr = list(current_arr)
-                    val1 = next_arr[i]
-                    val2 = next_arr[i + 1]
-                    del next_arr[i + 1]
-                    if turn == 0:  # Turtle's turn
-                        next_arr[i] = min(val1, val2)
-                        results.append(get_f_recursive(tuple(next_arr), 1))
-                    else:  # Piggy's turn
-                        next_arr[i] = max(val1, val2)
-                        results.append(get_f_recursive(tuple(next_arr), 0))
+    print(ND + ns)
 
-                if turn == 0:
-                    result = max(results)
-                else:
-                    result = min(results)
-                memo[current_tuple] = result
-                return result
+def main():
+    t = int(input())
+    for _ in range(t):
+        solve()
 
-            return get_f_recursive(tuple(arr), 0)
-
-        total_beauty = 0
-        for i in range(n):
-            for j in range(i, n):
-                subsegment = a[i : j + 1]
-                total_beauty += calculate_f(subsegment)
-        print(total_beauty)
-        return
-
-    def calculate_f_optimized(arr):
-        m = len(arr)
-        if m == 1:
-            return arr[0]
-        return arr[0] if m % 2 == 1 else min(arr[0], arr[-1])
-
-    def calculate_beauty(perm):
-        beauty = 0
-        for i in range(n):
-            for j in range(i, n):
-                subsegment = perm[i : j + 1]
-                beauty += calculate_f_optimized(subsegment)
-        return beauty
-
-    import itertools
-
-    max_beauty = 0
-    possible_fills = list(itertools.permutations(missing_numbers))
-
-    for fill in possible_fills:
-        temp_perm = list(a)
-        fill_idx = 0
-        for i in range(n):
-            if temp_perm[i] == 0:
-                temp_perm[i] = fill[fill_idx]
-                fill_idx += 1
-        max_beauty = max(max_beauty, calculate_beauty(temp_perm))
-
-    print(max_beauty)
-
-
-t = int(input())
-for _ in range(t):
-    solve()
+if __name__ == "__main__":
+    main()
--- 2084H-turtleAndNediam2.py
+++ 2084H-turtleAndNediam2.py
@@ -1,38 +1,128 @@
 # Author: AlifSrSE
 # Email: alif.rahman.c@gmail.com
 
-def solve():
-    n = int(input())
-    s = input()
-    mod = 10**9 + 7
+import sys
+input = sys.stdin.readline
 
-    distinct_sequences = set()
-    q = [(list(map(int, list(s))), 0)]
-    distinct_sequences.add(tuple(map(int, list(s))))
+MOD = 10 ** 9 + 7
+N = 1 << 21
 
-    while q:
-        current_s, operations = q.pop(0)
-        m = len(current_s)
+class Mint:
+    def __init__(self, x=0):
+        self.x = x % MOD
 
-        if operations < n - 2:
-            for i in range(m - 2):
-                subarray = sorted(current_s[i:i+3])
-                median = subarray[1]
-                j = -1
-                for k in range(i, m):
-                    if current_s[k] == median:
-                        j = k
-                        break
+    def __iadd__(self, other):
+        self.x = (self.x + other.x) % MOD
+        return self
 
-                if j != -1:
-                    next_s = current_s[:j] + current_s[j+1:]
-                    next_s_tuple = tuple(next_s)
-                    if next_s_tuple not in distinct_sequences:
-                        distinct_sequences.add(next_s_tuple)
-                        q.append((next_s, operations + 1))
+    def __isub__(self, other):
+        self.x = (self.x - other.x + MOD) % MOD
+        return self
 
-    print(len(distinct_sequences) % mod)
+    def __imul__(self, other):
+        self.x = (self.x * other.x) % MOD
+        return self
 
-t = int(input())
-for _ in range(t):
-    solve()
+    def __itruediv__(self, other):
+        return self.__imul__(other ** (MOD - 2))
+
+    def __add__(self, other):
+        return Mint(self.x).__iadd__(other)
+
+    def __sub__(self, other):
+        return Mint(self.x).__isub__(other)
+
+    def __mul__(self, other):
+        return Mint(self.x).__imul__(other)
+
+    def __truediv__(self, other):
+        return Mint(self.x).__itruediv__(other)
+
+    def __pow__(self, power):
+        result = Mint(1)
+        base = Mint(self.x)
+        while power:
+            if power & 1:
+                result *= base
+            base *= base
+            power >>= 1
+        return result
+
+    def __repr__(self):
+        return str(self.x)
+
+# Globals
+a = [0] * N
+ma = [0] * N
+dp = [Mint() for _ in range(N)]
+
+class StackSum:
+    def __init__(self):
+        self.stk = [0] * N
+        self.top = 0
+        self.sum = Mint(0)
+
+    def reset(self):
+        self.top = 0
+        self.sum = Mint(0)
+
+    def inc(self, i):
+        while self.top and ma[i] >= ma[self.stk[self.top]]:
+            self.top -= 1
+            if self.top:
+                delta = ma[self.stk[self.top]] - ma[self.stk[self.top + 1]]
+                self.sum -= Mint(delta) * dp[self.stk[self.top]]
+        self.stk[self.top + 1] = i
+        if self.top:
+            delta = ma[self.stk[self.top]] - ma[self.stk[self.top + 1]]
+            self.sum += Mint(delta) * dp[self.stk[self.top]]
+        self.top += 1
+        return self.sum
+
+ds = [StackSum(), StackSum()]
+
+def main_case():
+    length = int(input())
+    s = input().strip()[::-1]  
+    if all(c == s[0] for c in s):
+        print(len(s) - 1)
+        return
+
+    n = 0
+    len_block = 0
+    a_list = []
+    for i in range(len(s)):
+        len_block += 1
+        if i == len(s) - 1 or s[i] != s[i + 1]:
+            a[n + 1] = len_block
+            n += 1
+            len_block = 0
+
+    for i in range(n + 1):
+        dp[i] = Mint(0)
+
+    for i in range(1, n):
+        if (n - i) % 2 == 0:
+            dp[i] = Mint((n - i) // 2)
+        else:
+            dp[i] = Mint(a[n] + (n - i) // 2)
+
+    for i in range(1, n + 1):
+        ma[i] = a[i] + i // 2
+
+    ds[0].reset()
+    ds[1].reset()
+
+    for i in range(n, 1, -1):
+        dp[i - 1] += Mint(a[i]) * dp[i]
+        dp[i - 1] += ds[i & 1].inc(i)
+
+    print((dp[1] * Mint(a[1])).x)
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        main_case()
+
+if __name__ == "__main__":
+    main()
