a390db15c5893ea8aee4aad64751ee4ad62f2954
alifrahman <alif.rahman.c@gmail.com>
2025-06-19 09:21:34 +0600

    1218A-buddleReactor

--- 1218A-bubbleReactor.py
+++ 1218A-bubbleReactor.py
@@ -9,24 +9,20 @@ def solve():
     import sys
     input = sys.stdin.read
     data = input().split()
-    
     N = int(data[0])
     edges = data[1:]
     
     from collections import defaultdict
     tree = defaultdict(list)
     
-    # Build the adjacency list
     for i in range(0, len(edges), 2):
         u, v = int(edges[i]), int(edges[i+1])
         tree[u].append(v)
         tree[v].append(u)
-
     size = [0] * N
-    dp = [0] * N  # dp[u] = sum of subtree sizes rooted at u
+    dp = [0] * N
     ans = 0
 
-    # First DFS: calculate sizes and dp[0]
     def dfs1(u, parent):
         size[u] = 1
         for v in tree[u]:
@@ -35,23 +31,19 @@ def solve():
                 size[u] += size[v]
                 dp[u] += dp[v]
         dp[u] += size[u]
-
     dfs1(0, -1)
     ans = dp[0]
 
-    # Second DFS: re-rooting to calculate dp[u] for all u
     def dfs2(u, parent):
         nonlocal ans
         for v in tree[u]:
             if v != parent:
-                # move root from u to v
                 dp[v] = dp[u] - size[v] + (N - size[v])
                 size_v_orig = size[v]
-                size[v] = N  # when rerooted, v's size is total
+                size[v] = N
                 dfs2(v, u)
                 size[v] = size_v_orig
                 ans = max(ans, dp[v])
-
     dfs2(0, -1)
     print(ans)
 
