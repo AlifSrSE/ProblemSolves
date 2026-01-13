23b49a7ddee861c8077e14e432c73c4ef1897bf5
Alif Rahman <alif.rahman.c@gmail.com>
2026-01-13 22:39:05 +0600

    1447A-addCandies.py

---
+++ 1447A-addCandies.py
@@ -0,0 +1,19 @@
+
+
+#Author: AlifSrSE
+
+import sys
+
+def main():
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    t = int(input_data[0])
+    results = []
+    for i in range(1, t + 1):
+        n = int(input_data[i])
+        results.append(f"{n}\n{' '.join(map(str, range(1, n + 1)))}")
+    sys.stdout.write("\n".join(results) + "\n")
+
+if __name__ == "__main__":
+    main()
