af352debdb7cf02b2e5decb7389ab6b381ccca32
Alif Rahman <alif.rahman.c@gmail.com>
2026-02-01 22:58:22 +0600

    1455C-pingPong.py

---
+++ 1455C-pingPong.py
@@ -0,0 +1,11 @@
+# Author: AlifSrSE
+
+import sys
+
+def solve(x, y):
+    return f"{x - 1} {y}"
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    x, y = map(int, sys.stdin.readline().split())
+    print(solve(x, y))
